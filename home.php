208856e04c376329664f785c887a56dc8c2a26ee
muchamad2 <muhamadirsad146@gmail.com>
2019-01-15 05:22:43 +0700

    Project update, add connectio user to database

--- home.php
+++ home.php
@@ -4,17 +4,17 @@
 <title>PERATURAN GUBERNUR JAWA TIMUR</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="D:\Project\Web KOMINFO\cssHome.css">
+<link rel="stylesheet" type="text/css" href="cssHome.css">
 </head>
 
 <body>
     <header>
         <div class="gal-container">
             <div class="gal-logo">
-                <img src="D:\Project\Web KOMINFO\jatim.png" alt="logo" width="60" height="80">
+                <img src="jatim.png" alt="logo" width="60" height="80">
             </div>
             <nav>
-                <li><a href="registrasi.html">Registrasi</a></li>
+                <li><a href="registrasi.php">Registrasi</a></li>
                 <li><a href="login.html">Login</a></li>
                 <li><a href="tentang.html">Tentang</a></li>
                 <li><a href="kontak.html">Kontak</a></li>
