101c240c33a024c711ecd64b2d66fb6f204b1530
muchamad2 <muhamadirsad146@gmail.com>
2019-01-16 01:30:05 +0700

    user profile dashboard

--- users/profile.php
+++ users/profile.php
@@ -3,3 +3,19 @@ $query = "SELECT * FROM tbl_usr WHERE username = '$_SESSION['username']'";
 $result = mysqli_query($query);
 $data = mysqli_fetch_array($result);
  ?>
+
+ <div>
+ 	<div>
+ 		<div><h3>Profile</h3></div>
+ 		<div class="body-panel">
+ 			<div class="table-panel">
+ 				<table class="table profile-table">
+ 					<tr>
+ 						<td>Nama</td><td>:</td>
+ 						<td><<?php echo $data['name']; ?></td>
+ 					</tr>
+ 				</table>
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
