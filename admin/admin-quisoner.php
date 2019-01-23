dd0cf06d27b99cc3e9b2eff31678fa69fd25eab5
GaLHakim <galihalhakim15@gmail.com>
2019-01-23 09:10:34 +0700

    delete

--- admin/admin-quisoner.php
+++
@@ -1,7 +0,0 @@
-<div class="quis-judul">
-    <p>HALAMAN METODE INPUT QUISONER</p>
-</div>
-<div class="box-btn">
-    <input class="quis-btn isi" type="button" value="Isi Quisoner"></input>
-    <input class="quis-btn jawab" type="button" value="Isi Jawaban">
-</div>
